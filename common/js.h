b19514dae93f5bafef052d47310e715a749d814f
liudelong <1589075757@qq.com>
2022-10-15 06:54:40 -0700

    fix js.h bug

--- common/js.h
+++ common/js.h
@@ -20,7 +20,7 @@ class jser
         end_t=chrono::high_resolution_clock::now();
         auto time_span_ns=end_t-begin_t;
         chrono::duration<double> time_span=chrono::duration_cast<chrono::duration<double>>(end_t-begin_t);
-        return time_span_ns.count();
+        return time_span.count();
     }
     long long int end_ns()
     {
@@ -28,18 +28,19 @@ class jser
         auto time_span_ns=end_t-begin_t;
         return time_span_ns.count();
     }
-} timer;
+};
 
 #endif // JS_H
 /**
 int main()
 {
+    jser timer;
     timer.begin();
     int a;
     for(int i=0;i<100000;i++)
     {
         a++;
     }
-    cout<<timer.end();
+    cout<<timer.end_ns();
 }
 **/
